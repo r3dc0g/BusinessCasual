c0738387b39b494ead36dfda86efdeb949a6438a
AquillianFireblazer <gcrites.awesome@gmail.com>
2019-12-22 16:10:27 -0600

    played around with some stuff

--- application.py
+++ application.py
@@ -10,45 +10,12 @@ import arcade
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
-SCREEN_TITLE = "Move Keyboard Example"
+SCREEN_TITLE = "Business Casual"
 MOVEMENT_SPEED = 3
 
 
-class Ball:
-    def __init__(self, position_x, position_y, change_x, change_y, radius, color):
 
-        # Take the parameters of the init function above, and create instance variables out of them.
-        self.position_x = position_x
-        self.position_y = position_y
-        self.change_x = change_x
-        self.change_y = change_y
-        self.radius = radius
-        self.color = color
-
-    def draw(self):
-        """ Draw the balls with the instance variables we have. """
-        arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, self.color)
-
-    def update(self):
-        # Move the ball
-        self.position_y += self.change_y
-        self.position_x += self.change_x
-
-        # See if the ball hit the edge of the screen. If so, change direction
-        if self.position_x < self.radius:
-            self.position_x = self.radius
-
-        if self.position_x > SCREEN_WIDTH - self.radius:
-            self.position_x = SCREEN_WIDTH - self.radius
-
-        if self.position_y < self.radius:
-            self.position_y = self.radius
-
-        if self.position_y > SCREEN_HEIGHT - self.radius:
-            self.position_y = SCREEN_HEIGHT - self.radius
-
-
-class MyGame(arcade.Window):
+class BusinessCasual(arcade.Window):
 
     def __init__(self, width, height, title):
 
@@ -57,42 +24,57 @@ class MyGame(arcade.Window):
 
         # Make the mouse disappear when it is over the window.
         # So we just see our object, not the pointer.
-        self.set_mouse_visible(False)
+        self.set_mouse_visible(True)
+
+        self.player_list = None
+
+        self.player_sprite = None
 
         arcade.set_background_color(arcade.color.ASH_GREY)
+    
+    def setup(self):
+        
+        """ Set up the game here. Call this function to restart the game. """
+        # Create the Sprite lists
+        self.player_list = arcade.SpriteList()
 
-        # Create our ball
-        self.ball = Ball(50, 50, 0, 0, 15, arcade.color.AUBURN)
+        # Set up the player, specifically placing it at these coordinates.
+        image_source = os.path.join(os.path.dirname(__file__), "000.png")
+        self.player_sprite = arcade.Sprite(image_source, .25)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
 
     def on_draw(self):
         """ Called whenever we need to draw the window. """
         arcade.start_render()
-        self.ball.draw()
+        self.player_list.draw()
 
     def on_update(self, delta_time):
-        self.ball.update()
+        self.player_list.update()
 
     def on_key_press(self, key, modifiers):
         """ Called whenever the user presses a key. """
         if key == arcade.key.LEFT:
-            self.ball.change_x = -MOVEMENT_SPEED
+            self.player_sprite.change_x = -MOVEMENT_SPEED
         elif key == arcade.key.RIGHT:
-            self.ball.change_x = MOVEMENT_SPEED
+            self.player_sprite.change_x = MOVEMENT_SPEED
         elif key == arcade.key.UP:
-            self.ball.change_y = MOVEMENT_SPEED
+            self.player_sprite.change_y = MOVEMENT_SPEED
         elif key == arcade.key.DOWN:
-            self.ball.change_y = -MOVEMENT_SPEED
+            self.player_sprite.change_y = -MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
         """ Called whenever a user releases a key. """
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.ball.change_x = 0
+            self.player_sprite.change_x = 0
         elif key == arcade.key.UP or key == arcade.key.DOWN:
-            self.ball.change_y = 0
+            self.player_sprite.change_y = 0
 
 
 def main():
-    MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window = BusinessCasual(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
     arcade.run()
 
 

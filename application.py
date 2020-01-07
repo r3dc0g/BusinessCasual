7127c9ce8106b49ad01a2d7b5dee154690fab977
AquillianFireblazer <gcrites.awesome@gmail.com>
2020-01-07 16:48:24 -0600

    Garrett edited application.py

--- application.py
+++ application.py
@@ -1,26 +1,41 @@
 import os
 import sys
+import random
 
 parent_dir = os.path.abspath(os.path.dirname(__file__))
 vendor_dir = os.path.join(parent_dir, 'vendor')
 
 sys.path.append(vendor_dir)
 
+
 import arcade
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
+SPRITE_SCALING = .50
 SCREEN_TITLE = "Business Casual"
 MOVEMENT_SPEED = 3
-
+# How many pixels to keep as a minimum margin between the character
+# and the edge of the screen.
+LEFT_VIEWPORT_MARGIN = 200
+RIGHT_VIEWPORT_MARGIN = 200
+BOTTOM_VIEWPORT_MARGIN = 150
+TOP_VIEWPORT_MARGIN = 100
 
 
 class BusinessCasual(arcade.Window):
 
     def __init__(self, width, height, title):
 
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
+
         # Call the parent class's init function
         super().__init__(width, height, title)
+
+        
+        self.background = None
+        
 
         # Make the mouse disappear when it is over the window.
         # So we just see our object, not the pointer.
@@ -33,14 +48,16 @@ class BusinessCasual(arcade.Window):
         arcade.set_background_color(arcade.color.ASH_GREY)
     
     def setup(self):
-        
+        self.background = arcade.load_texture(os.getcwd() + "/Forest Playing Floor/Forest Background.png")
+
         """ Set up the game here. Call this function to restart the game. """
         # Create the Sprite lists
         self.player_list = arcade.SpriteList()
 
         # Set up the player, specifically placing it at these coordinates.
-        image_source = os.path.join(os.path.dirname(__file__), "000.png")
-        self.player_sprite = arcade.Sprite(image_source, .25)
+
+        image_source = os.path.join(__file__, os.getcwd() + "/Assets/Main Character Frames/000.png")
+        self.player_sprite = arcade.Sprite(image_source, SPRITE_SCALING)
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 128
         self.player_list.append(self.player_sprite)
@@ -48,11 +65,13 @@ class BusinessCasual(arcade.Window):
     def on_draw(self):
         """ Called whenever we need to draw the window. """
         arcade.start_render()
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 3, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
         self.player_list.draw()
 
     def on_update(self, delta_time):
         self.player_list.update()
 
+        
     def on_key_press(self, key, modifiers):
         """ Called whenever the user presses a key. """
         if key == arcade.key.LEFT:
@@ -80,3 +99,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
